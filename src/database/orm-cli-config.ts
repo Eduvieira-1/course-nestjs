6fee5e4e24698cf080481802e0f1b401df98dec7
Eduvieira-1 <edu.vieira1@icloud.com>
2024-02-14 21:29:16 -0300

    create table tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,3 +1,4 @@
+import { CreateTagsTable1707956415922 } from './../migrations/1707956415922-CreateTagsTable';
 import { CreateCoursesTable1707386984349 } from './../migrations/1707386984349-CreateCoursesTable';
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
@@ -5,5 +6,5 @@ import { dataSourceOptions } from "./database.module";
 export const dataSource = new DataSource({
     ...dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCoursesTable1707386984349]
+    migrations: [CreateCoursesTable1707386984349, CreateTagsTable1707956415922]
 })
