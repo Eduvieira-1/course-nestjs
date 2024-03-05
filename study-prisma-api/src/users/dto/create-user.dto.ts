2d2ac78693835ae00aaffd04af65a98d4905671a
Eduvieira-1 <edu.vieira1@icloud.com>
2024-03-04 23:12:12 -0300

    class-validator

--- study-prisma-api/src/users/dto/create-user.dto.ts
+++ study-prisma-api/src/users/dto/create-user.dto.ts
@@ -1,5 +1,19 @@
+import { IsEmail } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator/types/decorator/decorators';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsBoolean()
   admin: boolean;
 }
